f9d6ca09685b494c5f072716f03cb04c437ae781
mahbubnoyon506 <mahbubnoyon506@gmail.com>
2025-09-08 18:12:22 +0600

    Eslint config file updated

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
+import { FlatCompat } from "@eslint/eslintrc";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +10,11 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "eslint:recommended"
+  ),
   {
     ignores: [
       "node_modules/**",
@@ -19,6 +23,18 @@ const eslintConfig = [
       "build/**",
       "next-env.d.ts",
     ],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        React: "readonly",
+        google: "readonly",
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "no-unused-vars": "warn",
+    },
   },
 ];
 
